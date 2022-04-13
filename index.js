8f599857fcee447096e615c50fb91c49f47233e9
Peter Uadiale <peteuadiale1@gmail.com>
2022-04-13 21:30:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,3 +80,21 @@ $(".my-works").magnificPopup({
   gallery:{enabled:true}
  });
 }); 
+
+//Change active section on scroll
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop ) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
