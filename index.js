d0a9534f63835b4002c4f45a83dd564748469978
Peter Uadiale <peteuadiale1@gmail.com>
2022-06-10 16:01:59 +0100

    update index.html

--- index.js
+++ index.js
@@ -9,6 +9,52 @@ function menuToggle() {
 
 menu.onclick = menuToggle;
 
+// Typewriter Animation.
+var span = document.querySelector(".header-body span");
+var textArr = span.getAttribute("data-text").split(", ");
+console.log(textArr);
+var maxTextIndex = textArr.length;
+
+var sPerChar = 0.15;
+var sBetweenWord = 1.5;
+var textIndex = 0;
+
+typing(textIndex, textArr[textIndex]);
+
+function typing(textIndex, text) {
+  var charIndex = 0;
+  var maxCharIndex = text.length - 1;
+
+  var typeInterval = setInterval(function () {
+    span.innerHTML += text[charIndex];
+    if (charIndex == maxCharIndex) {
+      clearInterval(typeInterval);
+      setTimeout(function () {
+        deleting(textIndex, text);
+      }, sBetweenWord * 1000);
+    } else {
+      charIndex += 1;
+    }
+  }, sPerChar * 1000);
+}
+
+function deleting(textIndex, text) {
+  var minCharIndex = 0;
+  var charIndex = text.length - 1;
+
+  var typeInterval = setInterval(function () {
+    span.innerHTML = text.substr(0, charIndex);
+    if (charIndex == minCharIndex) {
+      clearInterval(typeInterval);
+      textIndex + 1 == maxTextIndex ? (textIndex = 0) : (textIndex += 1);
+      setTimeout(function () {
+        typing(textIndex, textArr[textIndex]);
+      }, sBetweenWord * 1000);
+    } else {
+      charIndex -= 1;
+    }
+  }, sPerChar * 1000);
+}
 
 // Active state of nav-links
 $('nav ul a').on('click', function(){
