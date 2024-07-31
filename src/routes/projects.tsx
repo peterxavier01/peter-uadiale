e3c683e88f40e26d4a2a19722849947c1fd06ca5
Peter Uadiale <fxavy02@gmail.com>
2024-07-31 20:56:05 +0100

    styles: modify component styles

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -12,10 +12,10 @@ export const Route = createFileRoute("/projects")({
 
 function ProjectsPage() {
   return (
-    <main className="my-16 min-h-[calc(100dvh-99px)] font-montserrat">
+    <main className="my-8 min-h-[calc(100dvh-99px)] font-montserrat md:my-16">
       <Heading title="My Projects" />
 
-      <section className="mt-12">
+      <section className="mt-12 flex flex-col gap-16 md:gap-24">
         {projects.map((project) => (
           <ProjectItem key={project.id} project={project} />
         ))}
