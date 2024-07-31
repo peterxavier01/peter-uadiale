780bcdb2a34f5dbca327d8ee1312176f152f2838
Peter Uadiale <fxavy02@gmail.com>
2024-07-31 15:14:57 +0100

    refactor: add project item component to projects page

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -2,6 +2,9 @@ import { createFileRoute } from "@tanstack/react-router";
 
 import Heading from "@/components/heading";
 import AnimatedOverlay from "@/components/animated-overlay";
+import ProjectItem from "@/components/project-item";
+
+import { projects } from "@/lib/data";
 
 export const Route = createFileRoute("/projects")({
   component: ProjectsPage,
@@ -9,8 +12,14 @@ export const Route = createFileRoute("/projects")({
 
 function ProjectsPage() {
   return (
-    <main className="min-h-[calc(100dvh-99px)] font-montserrat">
+    <main className="my-16 min-h-[calc(100dvh-99px)] font-montserrat">
       <Heading title="My Projects" />
+
+      <section className="mt-12">
+        {projects.map((project) => (
+          <ProjectItem key={project.id} project={project} />
+        ))}
+      </section>
 
       <AnimatedOverlay />
     </main>
