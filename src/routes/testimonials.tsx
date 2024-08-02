4f122718ffaa9217704bc7ca97864896356513b4
Peter Uadiale <fxavy02@gmail.com>
2024-08-02 16:52:21 +0100

    refactor: add ui for testimonial card layout

--- src/routes/testimonials.tsx
+++ src/routes/testimonials.tsx
@@ -1,6 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+
 import AnimatedOverlay from "@/components/animated-overlay";
 import Heading from "@/components/heading";
-import { createFileRoute } from "@tanstack/react-router";
+import TestimonialCard from "@/components/testimonial-card";
+
+import { testimonials } from "@/lib/data";
 
 export const Route = createFileRoute("/testimonials")({
   component: Testimonials,
@@ -8,8 +12,17 @@ export const Route = createFileRoute("/testimonials")({
 
 function Testimonials() {
   return (
-    <main className="min-h-[calc(100dvh-99px)] font-montserrat">
+    <main className="my-8 min-h-[calc(100dvh-99px)] font-montserrat md:my-16">
       <Heading title="Testimonials" />
+      <p className="mb-12 text-paragraph md:mb-16 md:text-h5">
+        Hear what my clients have to say
+      </p>
+
+      <section className="grid gap-8 md:grid-cols-2 md:gap-12">
+        {testimonials.map((testimonial) => (
+          <TestimonialCard key={testimonial.id} testimonial={testimonial} />
+        ))}
+      </section>
 
       <AnimatedOverlay />
     </main>
