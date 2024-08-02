1f4867e1be8d51178af3b27259d8d3b607a35300
Peter Uadiale <fxavy02@gmail.com>
2024-08-02 16:51:51 +0100

    refactor: add ui for contact card layout and contact form

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -2,6 +2,10 @@ import { createFileRoute } from "@tanstack/react-router";
 
 import Heading from "@/components/heading";
 import AnimatedOverlay from "@/components/animated-overlay";
+import ContactCard from "@/components/contact-card";
+import ContactForm from "@/components/contact-form";
+
+import { contactData } from "@/lib/data";
 
 export const Route = createFileRoute("/contact")({
   component: ContactPage,
@@ -9,8 +13,18 @@ export const Route = createFileRoute("/contact")({
 
 function ContactPage() {
   return (
-    <main className="min-h-[calc(100dvh-99px)] font-montserrat">
+    <main className="my-8 min-h-[calc(100dvh-99px)] font-montserrat md:my-16">
       <Heading title="Get In Touch" />
+
+      <section className="mt-8 grid gap-4 md:grid-cols-2 md:gap-10">
+        {contactData.map((data) => (
+          <ContactCard key={data.id} data={data} />
+        ))}
+      </section>
+
+      <section className="mt-[72px]">
+        <ContactForm />
+      </section>
 
       <AnimatedOverlay />
     </main>
