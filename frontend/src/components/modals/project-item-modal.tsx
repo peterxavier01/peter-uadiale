b2781be65619c36e0018f7ae4dafa3bdb077ee6a
Peter Uadiale <fxavy02@gmail.com>
2024-08-06 00:14:21 +0100

    feat: create modal component to showcase project details

---
+++ frontend/src/components/modals/project-item-modal.tsx
@@ -0,0 +1,97 @@
+import { useCallback, useEffect, useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import Tool from "@/components/tool";
+import SubHeading from "@/components/sub-heading";
+import List from "@/components/ui/list";
+
+import useProjectModalStore from "@/hooks/use-project-modal";
+
+import { projects } from "@/lib/data";
+
+import Demo from "../../assets/project-demo.png";
+
+export default function ProjectItemModal() {
+  const isOpen = useProjectModalStore((state) => state.isOpen);
+  const onClose = useProjectModalStore((state) => state.onClose);
+  const projectId = useProjectModalStore((state) => state.projectId);
+  const [project, setProject] = useState<(typeof projects)[0]>();
+
+  const getProjectById = useCallback(() => {
+    const project = projects.find((project) => project.id === projectId);
+    setProject(project);
+  }, [projectId]);
+
+  useEffect(() => {
+    getProjectById();
+  }, [getProjectById]);
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent className="no-scroll h-full w-full max-w-3xl space-y-4 overflow-y-auto border-dark-brown bg-dark-brown px-4 py-14 font-montserrat text-off-white max-sm:rounded-none md:max-h-[90vh] md:px-8">
+        <DialogHeader>
+          <DialogTitle className="mb-2 text-3xl font-bold">
+            {project?.name}
+          </DialogTitle>
+          <DialogDescription className="text-base text-off-white">
+            {project?.description}
+          </DialogDescription>
+        </DialogHeader>
+
+        <img
+          src={Demo ?? project?.image}
+          alt={project?.name}
+          className="block w-full"
+        />
+
+        <section>
+          <SubHeading title="Project Requirements" />
+          <div className="mt-4 space-y-4">
+            {project?.project_requirements?.map((item, index) => (
+              <List key={index} content={item} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <SubHeading title="My Approach" />
+          <div className="mt-4 space-y-4">
+            {project?.approach?.map((item, index) => (
+              <List key={index} content={item} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <SubHeading title="Challenges Faced" />
+          <div className="mt-4 space-y-4">
+            {project?.challenges?.map((item, index) => (
+              <List key={index} content={item} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <SubHeading title="Languages / Technologies used" />
+          <div className="mt-4 flex flex-wrap items-center gap-2">
+            {project?.tools.map((tool) => (
+              <Tool key={tool.id} name={tool.name} />
+            ))}
+          </div>
+        </section>
+      </DialogContent>
+    </Dialog>
+  );
+}
