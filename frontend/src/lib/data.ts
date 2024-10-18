c5e1e3e0f0ff2db03f2e95935aa52a742404e23f
Peter Uadiale <fxavy02@gmail.com>
2024-10-18 23:00:41 +0100

    refactor: modify portfolio project data

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -44,23 +44,31 @@ import Email from "@/components/icons/email";
 import Location from "@/components/icons/location";
 
 // Project Images
-import StyleVault from "../assets/style-vault.png";
-import StyleVault1 from "../assets/project-images/style-vault-2.png";
-import StyleVault2 from "../assets/project-images/style-vault.png";
-import StyleVault3 from "../assets/project-images/style-vault-metrics.png";
+// import StyleVault from "../assets/style-vault.png";
+// import StyleVault1 from "../assets/project-images/style-vault-2.png";
+// import StyleVault2 from "../assets/project-images/style-vault.png";
+// import StyleVault3 from "../assets/project-images/style-vault-metrics.png";
 
 import RealHome from "../assets/realhome-4.png";
 import RealHome1 from "../assets/project-images/realhome-2.png";
 import RealHome2 from "../assets/project-images/realhome.png";
-
-import OtterStream from "../assets/otterstream-2.png";
-import OtterStream1 from "../assets/project-images/otterstream.png";
-import OtterStream2 from "../assets/project-images/otterstream-2.png";
+import RealHome3 from "../assets/project-images/realhome-3.png";
+import RealHome4 from "../assets/project-images/realhome-4.png";
+import RealHome5 from "../assets/project-images/realhome-5.png";
+import RealHome6 from "../assets/project-images/realhome-6.png";
 
 import Brainy from "../assets/project-images/brainy.png";
 import Brainy1 from "../assets/project-images/brainy-02.png";
 import Brainy2 from "../assets/project-images/brainy-03.png";
 import Brainy3 from "../assets/project-images/brainy-04.png";
+
+import Portfolio from "../assets/portfolio.png";
+import Portfolio2 from "../assets/project-images/portfolio-02.png";
+import Portfolio3 from "../assets/project-images/portfolio-03.png";
+import Portfolio4 from "../assets/project-images/portfolio-04.jpeg";
+
+import SoundWave from "../assets/project-images/spotify-02.png";
+import SoundWave1 from "../assets/project-images/spotify-01.png";
 
 import { fadeInLeftVariants, fadeInRightVariants } from "./animations";
 
@@ -233,50 +241,50 @@ export const testimonials = [
 ];
 
 export const projects = [
-  {
-    id: 1,
-    image: StyleVault,
-    imageList: [StyleVault1, StyleVault2, StyleVault3],
-    name: "Style Vault",
-    category: "Ecommerce",
-    description:
-      "Style Vault is an e-commerce website project designed to offer a seamless shopping experience for clothes, shoes, and accessories for both men and women. This project leverages cutting-edge technologies to deliver a dynamic, responsive, and user-friendly platform.",
-    project_requirements: [
-      "Responsive website layout: The user should be able to view the optimal layout of the website depending on their device",
-      "Product catalog: To create a dynamic product catalog that is filterable by the user.",
-      "Shopping cart and checkout: Integrating a shopping cart and checkout system to facilitate the purchase process.",
-      "Payment gateway integration: To enable secure and convenient payment methods for users.",
-      "Dark mode support: To provide users the choice to use either a dark mode or light mode theme (defaults to system).",
-      "Payment gateway integration: To enable users to make purchases using various payment methods.",
-      "Search functionality: To allow users to search for products easily.",
-    ],
-    approach: [
-      "Component-based development: I used a component-based approach to build reusable and modular components for the website with React.",
-      "Mobile-first design: I followed a mobile-first design approach to ensure the website is optimized for mobile devices and provides a seamless experience on larger screens.",
-      "Headless CMS integration: I integrated a headless CMS (Commerce.js) to manage the product catalog, allowing for easy updates and modifications.",
-      "API-driven devlopment: I utilized the Commerce.js API to fetch product data, manage the shopping cart, and handle the checkout process.",
-      "Payment gateway solution: I implemented a payment gateway solution to enable users to make purchases for products by integrating the Paystack API.",
-    ],
-    challenges: [
-      "Performance optimization: I ensured the website loads quickly and efficiently. This involved optimizing images, implementing lazy loading, and using caching techniques.",
-      "User Experience (UX) design: I focused on creating an intuitive, minimalistic and user-friendly interface, ensuring that users can easily navigate the website and find the products they need.",
-      "Commerce.js CMS: I faced challenges in integrating the Commerce.js API into the project. This was because the latest version of Commerce.js used in this project was incompatible with Next 14 at the time of making this project, so I could not use features like server actions to reduce client JS and I also ran into other errors. To rectify this, I had to downgrade Next.js to version 13.5.3.",
-    ],
-    tools: [
-      { id: 1, icon: Next, name: "Next.js" },
-      { id: 2, icon: Framer, name: "Framer Motion" },
-      { id: 3, icon: Typescript, name: "Typescript" },
-      { id: 4, icon: Tailwind, name: "Tailwind" },
-      { id: 5, name: "Paystack" },
-      { id: 6, name: "Commerce.js" },
-    ],
-    live_preview_url: "https://style-vault.vercel.app/",
-    github_repo_url: "https://github.com/peterxavier01/style-vault",
-  },
+  // {
+  //   id: 1,
+  //   image: StyleVault,
+  //   imageList: [StyleVault1, StyleVault2, StyleVault3],
+  //   name: "Style Vault",
+  //   category: "Ecommerce",
+  //   description:
+  //     "Style Vault is an e-commerce website project designed to offer a seamless shopping experience for clothes, shoes, and accessories for both men and women. This project leverages cutting-edge technologies to deliver a dynamic, responsive, and user-friendly platform.",
+  //   project_requirements: [
+  //     "Responsive website layout: The user should be able to view the optimal layout of the website depending on their device",
+  //     "Product catalog: To create a dynamic product catalog that is filterable by the user.",
+  //     "Shopping cart and checkout: Integrating a shopping cart and checkout system to facilitate the purchase process.",
+  //     "Payment gateway integration: To enable secure and convenient payment methods for users.",
+  //     "Dark mode support: To provide users the choice to use either a dark mode or light mode theme (defaults to system).",
+  //     "Payment gateway integration: To enable users to make purchases using various payment methods.",
+  //     "Search functionality: To allow users to search for products easily.",
+  //   ],
+  //   approach: [
+  //     "Component-based development: I used a component-based approach to build reusable and modular components for the website with React.",
+  //     "Mobile-first design: I followed a mobile-first design approach to ensure the website is optimized for mobile devices and provides a seamless experience on larger screens.",
+  //     "Headless CMS integration: I integrated a headless CMS (Commerce.js) to manage the product catalog, allowing for easy updates and modifications.",
+  //     "API-driven devlopment: I utilized the Commerce.js API to fetch product data, manage the shopping cart, and handle the checkout process.",
+  //     "Payment gateway solution: I implemented a payment gateway solution to enable users to make purchases for products by integrating the Paystack API.",
+  //   ],
+  //   challenges: [
+  //     "Performance optimization: I ensured the website loads quickly and efficiently. This involved optimizing images, implementing lazy loading, and using caching techniques.",
+  //     "User Experience (UX) design: I focused on creating an intuitive, minimalistic and user-friendly interface, ensuring that users can easily navigate the website and find the products they need.",
+  //     "Commerce.js CMS: I faced challenges in integrating the Commerce.js API into the project. This was because the latest version of Commerce.js used in this project was incompatible with Next 14 at the time of making this project, so I could not use features like server actions to reduce client JS and I also ran into other errors. To rectify this, I had to downgrade Next.js to version 13.5.3.",
+  //   ],
+  //   tools: [
+  //     { id: 1, icon: Next, name: "Next.js" },
+  //     { id: 2, icon: Framer, name: "Framer Motion" },
+  //     { id: 3, icon: Typescript, name: "Typescript" },
+  //     { id: 4, icon: Tailwind, name: "Tailwind" },
+  //     { id: 5, name: "Paystack" },
+  //     { id: 6, name: "Commerce.js" },
+  //   ],
+  //   live_preview_url: "https://style-vault.vercel.app/",
+  //   github_repo_url: "https://github.com/peterxavier01/style-vault",
+  // },
   {
     id: 2,
     image: RealHome,
-    imageList: [RealHome1, RealHome2],
+    imageList: [RealHome1, RealHome2, RealHome6, RealHome3, RealHome4, RealHome5],
     name: "RealHome",
     category: "Web App",
     description:
@@ -326,9 +334,14 @@ export const projects = [
     category: "AI",
     description:
       "Brainy is an AI application with a modern and minimalistic UI which incorporates the latest AI capabilites to provide tools for image generation, music generation, code generation and even video generation, to say the least.",
-    project_requirements: ["", ""],
-    approach: ["", ""],
-    challenges: ["", ""],
+    project_requirements: ["Responsive web app layout", "Admin dashboard integration", "Integrate AI for image generation", "Integrate AI for music generation", "Integrate AI for code generation", "Integrate AI for video generation"],
+    approach: ["Mobile-first design", "Component-based development", "Integrate OpenAI API for AI capabilities", "Integrate Replicate API for AI capabilities", "Integrate UploadThing for file upload", "Integrate LemonSqueezy for subscription management", "Integrate Clerk for authentication and authorization", "Integrate Prisma for database management"],
+    challenges: [
+      "Integrating multiple AI capabilities",
+      "Handling subscription management: I used LemonSqueezy for the first time in this project, instead of Stripe. Since it is fairly new, there were no much resources available when bugs were encountered.",
+      "Handling file upload: This was my first time working with UploadThing, so I had to go through the docs to figure out how to integrate it with Next.js",
+      "Handling authentication and authorization"
+    ],
     tools: [
       { id: 1, icon: Next, name: "Next.js" },
       { id: 2, icon: Typescript, name: "Typescript" },
@@ -348,24 +361,70 @@ export const projects = [
   },
   {
     id: 4,
-    image: OtterStream,
-    imageList: [OtterStream1, OtterStream2],
-    name: "Otter Stream",
-    category: "Web App",
-    description:
-      "Otter Stream is a fast, performance-optimized video streaming platform with a clean UI. It integrates the Youtube API to display its videos via a powerful video player library.",
-    project_requirements: ["", ""],
-    approach: ["", ""],
-    challenges: ["", ""],
+    image: Portfolio,
+    imageList: [Portfolio2, Portfolio3, Portfolio4],
+    name: "Portfolio",
+    category: "Web",
+    description: "This portfolio integrates a clean and minimalistic UI to showcase my frontend development projects.",
+    project_requirements: [
+      "The website should have a responsive layout", 
+      "The website should incorporate subtle animations", 
+      "Potential clients should be able to send messages/emails via the contact form", 
+      "The website should show a list of my projects",
+      "The projects should contain links to the live preview and the respective GitHub repositories."
+    ],
+    approach: [
+      "Mobile-first design", 
+      "Component-based development", 
+      "Framer Motion Integration for animations", 
+      "Resend Integration for contact form", 
+    ],
+    challenges: [
+      "Integrating Resend for contact form: This was a first working with Resend as an email soluion. I integrated Resend on the Node.js server. Resend requires an active domain which I had to purchase, and then set up the necessary SPF, DKIM, and DMARC records to ensure email deliverability.",
+    ],
     tools: [
       { id: 1, icon: React, name: "React.js" },
-      { id: 2, icon: MaterialUI, name: "Material UI" },
-      { id: 3, name: "JavaScript" },
-      { id: 4, name: "React Query" },
+      { id: 4, name: "Tailwind CSS" },
+      { id: 2, name: "Shadcn UI" },
+      { id: 3, name: "TypeScript" },
       { id: 5, name: "Axios" },
-      { id: 6, name: "React Player" },
+      { id: 6, name: "Zustand" },
+      { id: 7, name: "Resend" },
+      { id: 8, name: "Express" },
+      { id: 9, name: "Figma" },
     ],
-    live_preview_url: "https://otterstream.vercel.app/",
-    github_repo_url: "https://github.com/peterxavier01/otter-stream",
+    live_preview_url: "https://www.peteruadiale.com",
+    github_repo_url: "https://github.com/peterxavier01/peter-uadiale",
+  },
+  {
+    id: 5,
+    image: SoundWave,
+    imageList: [SoundWave, SoundWave1],
+    name: "SoundWave",
+    category: "Web App",
+    description: "SoundWave is a music player web app that allows users to listen to their favorite songs, create playlists, and search for songs.",
+    project_requirements: [
+      "Users should be able to view the optimal layout of the app depending on their device.", 
+      "Users should be able to create an account to personalize experience.", 
+      "Users should be able to search for songs.",
+      "Users should be able to add any song to their playlist",
+      "User should be able to subscribe to service and manage their subscriptions.",
+    ],
+    approach: ["Mobile-first design", "Component-based development", "Supabase Integration for managing songs data, authentication and authorization.", "Stripe Integration for subscription management"],
+    challenges: [
+      "Supabase Integration: It was quite tricky working with Supabase as I had just transitioned from Firebase (NoSql to SQL). The major struggle was dealing with errors that arose from implementing RLS (Row Level Access) to the Supabase tables.", 
+      "Stripe Integration: I had to go through the Stripe docs to figure out how to integrate it with Next.js. I also had to figure out how to manage subscriptions and handle webhooks.",
+    ],
+    tools: [
+      { id: 1, icon: Next, name: "Next.js" },
+      { id: 2, name: "Supabase" },
+      { id: 3, name: "TypeScript" },
+      { id: 4, name: "Tailwind CSS" },
+      { id: 5, name: "React Hook Form" },
+      { id: 7, name: "Stripe" },
+      { id: 6, name: "Zustand" },
+    ],
+    live_preview_url: "https://soundwave-alpha.vercel.app",
+    github_repo_url: "https://github.com/peterxavier01/spotify-clone-v1",
   },
 ];
