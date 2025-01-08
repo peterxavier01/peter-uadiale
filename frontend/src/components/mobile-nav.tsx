bc151a5b7ca33832fc7fdb95db99ae3ebc2f7276
Peter Uadiale <fxavy02@gmail.com>
2025-01-08 19:39:42 +0100

    refactor: add social icons to mobile nav component

--- frontend/src/components/mobile-nav.tsx
+++ frontend/src/components/mobile-nav.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Menu } from "lucide-react";
+import { Link } from "@tanstack/react-router";
 
 import { Button } from "@/components/ui/button";
 import NavItems from "@/components/nav-items";
@@ -12,12 +13,17 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
+import { socialIcons } from "@/lib/data";
+
 export default function MobileNav() {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
     <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger asChild className="block md:hidden">
+      <SheetTrigger
+        asChild
+        className="block hover:bg-transparent hover:text-white md:hidden"
+      >
         <Button variant="ghost">
           <Menu />
         </Button>
@@ -32,6 +38,25 @@ export default function MobileNav() {
 
         <div className="mt-8 flex flex-col gap-6">
           <NavItems setOpen={setOpen} />
+        </div>
+
+        <div className="z-10 mt-8 flex w-full gap-8 md:col-span-4 md:ml-auto md:hidden md:w-max md:flex-col">
+          {socialIcons.map((item) => {
+            const Icon = item.icon;
+
+            return (
+              <div key={item.id}>
+                <Link
+                  to={item.href}
+                  key={item.id}
+                  target="_blank"
+                  title={item.name}
+                >
+                  <Icon fill="var(--off-white)" className="social-icon" />
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </SheetContent>
     </Sheet>
